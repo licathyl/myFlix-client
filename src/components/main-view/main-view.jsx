11db11f1bb64932344586ececf0ad18958225a77
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-15 13:49:36 -0700

    Import axios and add get request

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from 'axios';
+
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -6,28 +8,21 @@ class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: "Inception",
-          Description: "desc1...",
-          ImagePath: "...",
-        },
-        {
-          _id: 2,
-          Title: "The Shawshank Redemption",
-          Description: "desc2...",
-          ImagePath: "...",
-        },
-        {
-          _id: 3,
-          Title: "Gladiator",
-          Description: "desc3...",
-          ImagePath: "...",
-        },
-      ],
-      selectedMovie: null,
+      movies: [],
+      selectedMovie: null
     };
+  }
+
+  componentDidMount(){
+    axios.get('https://cathysflix.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data 
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -40,14 +35,14 @@ class MainView extends React.Component {
     const { movies, selectedMovie } = this.state;
 
     if (movies.length === 0)
-      return <div className="main-view">This list is empty!</div>;
+      return <div className="main-view" />;
 
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
           ))
         }
       </div>
