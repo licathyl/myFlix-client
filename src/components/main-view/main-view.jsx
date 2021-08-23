43547a3174c2d4bfdf9bc2be8a41fa6ba5d0d5a3
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-23 14:10:50 -0700

    Update MovieCard Col component styling

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -72,8 +72,8 @@ class MainView extends React.Component {
             </Col> 
           )
           : movies.map(movie => (
-            <Col md={3}>
-              <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+            <Col sm={12} md={6} lg={4} key={movie._id} className="mb-4">
+              <MovieCard movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
             </Col> 
           ))
         }
