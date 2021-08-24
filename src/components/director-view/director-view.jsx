b3b615961549931c057d8157bfde9b0362cda179
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-23 17:42:56 -0700

    Add DirectorView Bootstrap component

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export class DirectorView extends React.Component {
+
+  render() {
+    const { director, onBackClick } = this.props;
+
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title>{director.Name}</Card.Title>
+          <Card.Text>{director.Bio}</Card.Text>
+          <Card.Text>Born: {director.Birth}</Card.Text>
+        </Card.Body>
+        <Button onClick={() => { onBackClick(null); }} variant="primary">Back</Button>
+      </Card>
+    );
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+  }).isRequired,
+};
