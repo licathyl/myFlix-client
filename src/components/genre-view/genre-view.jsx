953a36d6056aba80f22c3adb9c5d0f62e817d95e
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-23 17:42:38 -0700

    Add GenreView Bootstrap component

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { genre, onBackClick } = this.props;
+
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title>{genre.Name}</Card.Title>
+          <Card.Text>{genre.Description}</Card.Text>
+        </Card.Body>
+        <Button onClick={() => { onBackClick(null); }} variant="primary">Back</Button>
+      </Card>
+    );
+  }
+}
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+  }).isRequired,
+};
