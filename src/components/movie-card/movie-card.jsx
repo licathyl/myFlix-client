cb5089f264fde78a0d5316ed38dea31c64f3f1dc
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-23 17:40:43 -0700

    Update Bootstrap card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,9 +3,11 @@ import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
       <Card>
@@ -13,7 +15,9 @@ export class MovieCard extends React.Component {
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="primary">Open</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="primary">Open</Button>
+          </Link>
         </Card.Body>
       </Card>
     );
@@ -26,5 +30,5 @@ MovieCard.propTypes = {
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  // onMovieClick: PropTypes.func.isRequired
 };
