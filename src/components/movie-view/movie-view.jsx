c0966fbe630c8c4cff53b415948e2f89d2e37545
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-31 21:09:38 -0700

    Update movie view component

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,23 +1,69 @@
 import React from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+import { Link } from "react-router-dom";
+import "./movie-view.scss";
+
 export class MovieView extends React.Component {
+  handleAdd() {
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("user");
+
+    axios
+      .post(
+        `https://cathysflix.herokuapp.com/users/${username}/movies/${this.props.movie._id}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        alert("Added to Favorites List");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Card>
+      <Card className="movie-view">
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Card.Link href={`/directors/${movie.Director.Name}`}>Director</Card.Link>
-          <Card.Link href={`/genres/${movie.Genre.Name}`}>Genre</Card.Link>
+          <p>
+            Genre:&nbsp;
+            <Link to={`/genres/${movie.Genre.Name}`}>{movie.Genre.Name}</Link>
+          </p>
+          <p>
+            Director:&nbsp;
+            <Link to={`/directors/${movie.Director.Name}`}>
+              {movie.Director.Name}
+            </Link>
+          </p>
+          <Button
+            variant="danger"
+            className="fav-button"
+            value={movie._id}
+            onClick={(e) => this.handleAdd(e, movie)}
+          >
+            Add to Favorites
+          </Button>
+          <Button
+            onClick={() => {
+              onBackClick(null);
+            }}
+            variant="primary"
+          >
+            Back
+          </Button>
         </Card.Body>
-        <Button onClick={() => { onBackClick(null); }} variant="primary">Back</Button>
       </Card>
     );
   }
