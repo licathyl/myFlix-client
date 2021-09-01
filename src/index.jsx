5b7b2cbde788eaa083b38e1d75786f4bd9a85546
licathyl <83192907+licathyl@users.noreply.github.com>
2021-08-31 21:07:41 -0700

    Remove nav bar

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainView from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 
 // Import statement to indicate that you need to bundle './index.scss'
 import "./index.scss";
@@ -12,17 +11,6 @@ class MyFlixApplication extends React.Component {
   render() {
     return (
       <Container>
-        <Nav variant="pills" className="justify-content-end" defaultActiveKey="/home">
-          <Nav.Item>
-            <Nav.Link href="#home">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2">Option 3</Nav.Link>
-          </Nav.Item>
-        </Nav>
         <MainView />
       </Container> 
     );
