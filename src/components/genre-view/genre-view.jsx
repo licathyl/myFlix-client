6486d5c3004f3e5f7c901e6c1e01c5d537eb5395
licathyl <83192907+licathyl@users.noreply.github.com>
2021-09-06 14:22:46 -0700

    Update genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import "./genre-view.scss";
 
 export class GenreView extends React.Component {
   render() {
-    const { genre, onBackClick } = this.props;
+    const { genre, movies, onBackClick } = this.props;
+
+    const genreMovies = movies.filter((m) => m.Genre.Name === genre.Name);
 
     return (
-      <Card>
-        <Card.Body>
-          <Card.Title>{genre.Name}</Card.Title>
-          <Card.Text>{genre.Description}</Card.Text>
-        </Card.Body>
-        <Button
-          onClick={() => {
-            onBackClick(null);
-          }}
-          variant="primary"
-        >
-          Back
-        </Button>
-      </Card>
+      <Container className="m-4">
+        <Card>
+          <Card.Body>
+            <Card.Title>{genre.Name}</Card.Title>
+            <Card.Text>{genre.Description}</Card.Text>
+            <Button
+              onClick={() => {
+                onBackClick(null);
+              }}
+              variant="primary"
+            >
+              Back
+            </Button>
+          </Card.Body>
+          <Card.Body>
+            <Card.Title>Related Movies</Card.Title>
+            <Row>
+              {genreMovies.map((m, i) => (
+                <Link to={`/movies/${m._id}`} className="genre-movies" key={i}>
+                  {m.Title}
+                </Link>
+              ))}
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   }
 }
