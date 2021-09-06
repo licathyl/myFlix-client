4ae935851e25767f8948b2e85e847ee086c682dc
licathyl <83192907+licathyl@users.noreply.github.com>
2021-09-06 14:24:52 -0700

    Update director component

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,28 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import "./director-view.scss";
 
 export class DirectorView extends React.Component {
   render() {
-    const { director, onBackClick } = this.props;
+    const { director, movies, onBackClick } = this.props;
+
+    const directorsMovies = movies.filter(
+      (m) => m.Director.Name === director.Name
+    );
 
     return (
-      <Card>
-        <Card.Body>
-          <Card.Title>{director.Name}</Card.Title>
-          <Card.Text>{director.Bio}</Card.Text>
-          <Card.Text>Born: {director.Birth}</Card.Text>
-        </Card.Body>
-        <Button
-          onClick={() => {
-            onBackClick(null);
-          }}
-          variant="primary"
-        >
-          Back
-        </Button>
-      </Card>
+      <Container>
+        <Card>
+          <Card.Body>
+            <Card.Title>{director.Name}</Card.Title>
+            <Card.Text>{director.Bio}</Card.Text>
+            <Card.Text>Born: {director.Birth}</Card.Text>
+            <Button
+              onClick={() => {
+                onBackClick(null);
+              }}
+              variant="primary"
+            >
+              Back
+            </Button>
+          </Card.Body>
+          <Card.Body>
+            <Card.Title>Related Movies</Card.Title>
+            <Row>
+              <Col>
+                {directorsMovies.map((m, i) => (
+                  <Link
+                    to={`/movies/${m._id}`}
+                    className="directors-movies"
+                    key={i}
+                  >
+                    {m.Title}
+                  </Link>
+                ))}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   }
 }
