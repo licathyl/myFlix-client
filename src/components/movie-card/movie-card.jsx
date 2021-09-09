7207ab57d730cbc3714ab4b1b8a956482077ee19
licathyl <83192907+licathyl@users.noreply.github.com>
2021-09-08 21:57:06 -0700

    Add component styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,23 +3,32 @@ import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+import "./movie-card.scss";
+
 import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    return (
-      <Card>
+    return ["Dark"].map((variant, idx) => (
+      <Card
+        className="mb-4"
+        style={{ backgroundColor: "#141414" }}
+        border="success"
+        bg={variant.toLowerCase()}
+        key={idx}
+        text={variant.toLowerCase() === "light" ? "dark" : "white"}
+      >
         <Card.Img src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
           <Link to={`/movies/${movie._id}`}>
-            <Button variant="primary">Open</Button>
+            <Button variant="success">Open</Button>
           </Link>
         </Card.Body>
       </Card>
-    );
+    ));
   }
 }
 
