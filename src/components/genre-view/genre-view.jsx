083872c9e5759a2c7955c30b95f355b76f93422b
licathyl <83192907+licathyl@users.noreply.github.com>
2021-09-08 21:54:19 -0700

    Add component styling

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -11,34 +11,38 @@ export class GenreView extends React.Component {
 
     const genreMovies = movies.filter((m) => m.Genre.Name === genre.Name);
 
-    return (
-      <Container className="m-4">
-        <Card>
-          <Card.Body>
-            <Card.Title>{genre.Name}</Card.Title>
-            <Card.Text>{genre.Description}</Card.Text>
-            <Button
-              onClick={() => {
-                onBackClick(null);
-              }}
-              variant="primary"
-            >
-              Back
-            </Button>
-          </Card.Body>
-          <Card.Body>
-            <Card.Title>Related Movies</Card.Title>
-            <Row>
-              {genreMovies.map((m, i) => (
-                <Link to={`/movies/${m._id}`} className="genre-movies" key={i}>
-                  {m.Title}
-                </Link>
-              ))}
-            </Row>
-          </Card.Body>
-        </Card>
-      </Container>
-    );
+    return ["Dark"].map((variant, idx) => (
+      <Card
+        className="mb-4"
+        border="success"
+        bg={variant.toLowerCase()}
+        key={idx}
+        text={variant.toLowerCase() === "light" ? "dark" : "white"}
+      >
+        <Card.Body>
+          <Card.Title>{genre.Name}</Card.Title>
+          <Card.Text>{genre.Description}</Card.Text>
+          <Button
+            onClick={() => {
+              onBackClick(null);
+            }}
+            variant="primary"
+          >
+            Back
+          </Button>
+        </Card.Body>
+        <Card.Body>
+          <Card.Title>Related Movies</Card.Title>
+          <Row>
+            {genreMovies.map((m, i) => (
+              <Link to={`/movies/${m._id}`} className="genre-movies" key={i}>
+                {m.Title}
+              </Link>
+            ))}
+          </Row>
+        </Card.Body>
+      </Card>
+    ));
   }
 }
 
